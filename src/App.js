a0bfce2ca463de539f05bd525cd2d56a4331ad4d
Arin <arinmandal40@gmail.com>
2023-09-11 02:29:43 +0530

    CRA & Tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-slate-500">
+     <h1 className='text-3xl font-extrabold text-yellow-500'>MazeOfMovies🍿</h1>
     </div>
   );
 }
