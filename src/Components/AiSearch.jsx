8b27ae4cca8ad984ae4fd6955a628ebbccd0f19d
Arin <arinmandal40@gmail.com>
2025-03-23 01:12:01 +0530

    feat: Added heading, Gemini model, and improved search component design.

---
+++ src/Components/AiSearch.jsx
@@ -0,0 +1,11 @@
+import AiSearchBar from "./AiSearchBar"
+import AiSuggestions from "./AiSuggestions"
+const AiSearch = () => {
+  return (
+    <>
+      <AiSearchBar />
+      <AiSuggestions />
+    </>
+  )
+}
+export default AiSearch
